5189828bc1605dc83a7aae3a14366d8df297bbb3
Giselli Hara <gi@hara.ninja>
2021-07-12 22:02:01 -0300

    ajustando o primeiro layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+/*Reset do CSS */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}  
+body {
+    font-family: sans-serif;
+    background-color: #D9e6f6;
   }
+
+#_next{
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+img{
+  max-width: 100%;
+  height: auto;
+  display: block;  
+}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'green',
   },
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`;
+
+const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  padding: 16px;
+  .profileArea {
+    display: none;
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media (min-width: 860px) {
+    max-width: 1100px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+        <Box>
+          <img src="https://github.com/giselli.png" />
+        </Box>
+      </div>
+
+      <div>
+        <Box className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          Bem-vindo
+        </Box>
+      </div>
+
+      <div
+        className="profileRelationsArea"
+        style={{ gridArea: "profileRelationsArea" }}
+      >
+        <Box>Comunidades</Box>
+        <Box>Pessoas da Comunidade</Box>
+      </div>
+    </MainGrid>
+  );
 }
